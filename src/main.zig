ffa599ddc3c4b47c0cc66bc48c12ce04b74becae
HaraldWik <wikhallharald@gmail.com>
2025-10-14 19:49:54 +0200

    Minor cleanup

--- src/main.zig
+++ src/main.zig
@@ -6,65 +6,80 @@ const physics = @import("physics.zig");
 const ecs = @import("ecs");
 const Render = @import("render.zig");
 
-pub const CPlayer = extern struct {
-    identity: [32]u8,
-    player_id: u32,
-    name: [*:0]const u8, // C string pointer
-    position: nz.Vec3(f32),
-    rotation: nz.Vec3(f32),
-    direction: nz.Vec3(f32),
-};
-
 pub const db = struct {
-    pub const Connection = opaque {
-        pub fn connect() !*@This() {
-            return c.connect_to_db_ffi() orelse error.Connect;
-        }
-        pub fn disconnect(self: *@This()) void {
+    pub const Callback = union(enum) {
+        pub const PlayerConnect = *const fn (*Player, *World) callconv(.c) void;
+
+        player_connect: PlayerConnect,
+    };
+
+    pub fn connect() !Connection {
+        return c.connect_to_db_ffi() orelse error.Connect;
+    }
+
+    pub const Connection = *opaque {
+        pub fn close(self: *@This()) void {
             c.free_db_connection(self);
         }
-        pub fn subscribe_to_tables(self: *@This()) void {
+
+        pub fn subscribeToTables(self: *@This()) void {
             c.db_subscribe_to_tables(self);
         }
-        pub fn run_threaded(self: *@This()) void {
+
+        pub fn runThreaded(self: *@This()) void {
             c.db_run_threaded(self);
+        }
+
+        pub fn setCallback(self: *@This(), callback: Callback, world: *World) void {
+            switch (callback) {
+                .player_connect => |player_connect| c.register_player_connect_callback(
+                    self,
+                    world,
+                    player_connect,
+                ),
+            }
+        }
+    };
+
+    pub const Player = extern struct {
+        identity: [32]u8,
+        player_id: u32,
+        name: [*:0]const u8, // C string pointer
+        position: nz.Vec3(f32),
+        rotation: nz.Vec3(f32),
+        direction: nz.Vec3(f32),
+
+        pub fn deinit(self: @This()) void {
+            c.free_cplayer(self);
         }
     };
 
     pub const c = struct {
+        pub extern fn connect_to_db_ffi() callconv(.c) ?Connection;
+        pub extern fn free_db_connection(connection: Connection) callconv(.c) void;
+
         pub extern fn register_player_connect_callback(
-            connection: ?*Connection,
-            game_state: ?*World,
-            callback: ?*const fn (player: ?*CPlayer, world: ?*World) callconv(.c) void,
+            connection: Connection,
+            world: *World,
+            callback: Callback.PlayerConnect,
         ) void;
-        extern fn free_cplayer(p: ?*CPlayer) void;
-        pub extern fn connect_to_db_ffi() callconv(.c) ?*Connection;
-        pub extern fn free_db_connection(connection: ?*Connection) callconv(.c) void;
-        pub extern fn db_subscribe_to_tables(connection: ?*Connection) callconv(.c) void;
-        pub extern fn db_run_threaded(connection: ?*Connection) callconv(.c) void;
+
+        pub extern fn db_subscribe_to_tables(connection: Connection) callconv(.c) void;
+        pub extern fn db_run_threaded(connection: Connection) callconv(.c) void;
+
+        pub extern fn free_cplayer(p: *Player) void;
     };
 };
 
 // your callback that Rust will call
-pub fn player_connect(p: ?*CPlayer, world: ?*World) callconv(.c) void {
-    std.debug.print("ZULULUL\n", .{});
-    if (world != null) {
-        std.debug.print("ZULULUL2\n", .{});
+pub fn playerConnect(player: *db.Player, world: *World) callconv(.c) void {
+    std.debug.print("Player connected:\n", .{});
+    std.debug.print("\tid: {}\n", .{player.player_id});
+    std.debug.print("\tname: {s}\n", .{player.name});
+    std.debug.print("\tpos: ({}, {}, {})\n", .{ player.position[0], player.position[1], player.position[2] });
 
-        const e = world.?.*.add() catch return;
-        e.set(nz.Transform3D(f32), .{}, world.?.*);
-    }
-    std.debug.print("ZULULUL4\n", .{});
-
-    if (p) |player| {
-        const name = std.mem.span(player.name);
-        std.debug.print("Player connected:\n", .{});
-        std.debug.print("  id: {}\n", .{player.player_id});
-        std.debug.print("  name: {s}\n", .{name});
-        std.debug.print("  pos: ({}, {}, {})\n", .{ player.position[0], player.position[1], player.position[2] });
-    } else {
-        std.debug.print("player_connect called with null\n", .{});
-    }
+    const e = world.*.add() catch return;
+    e.set(nz.Transform3D(f32), .{}, world.*);
     // std.debug.print("TOT {d}:\n", .{world.?.generation.items.len});
 }
 
@@ -77,13 +92,13 @@ pub fn main() !void {
     var world: World = try .init(allocator, null);
     defer world.deinit();
 
-    const connection: *db.Connection = try .connect();
-    defer connection.disconnect();
+    const connection: db.Connection = try db.connect();
+    defer connection.close();
 
-    db.c.register_player_connect_callback(connection, &world, player_connect);
-    connection.subscribe_to_tables();
+    connection.setCallback(.{ .player_connect = playerConnect }, &world);
+    connection.subscribeToTables();
 
-    connection.run_threaded();
+    connection.runThreaded();
 
     std.Thread.sleep(3000);
 
