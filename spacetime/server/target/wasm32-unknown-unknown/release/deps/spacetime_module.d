3f5717712c32ee191b57bfe7b0f48c014e2133d1
Lucas Eklund <110255570+LuEklund@users.noreply.github.com>
2025-10-19 22:09:35 +0300

    Delete spacetime/server/target/wasm32-unknown-unknown/release/deps/spacetime_module.d

--- spacetime/server/target/wasm32-unknown-unknown/release/deps/spacetime_module.d
+++
@@ -1,7 +0,0 @@
-/home/admin/projects/PlanetaryZigma/spacetime/server/target/wasm32-unknown-unknown/release/deps/spacetime_module.d: src/lib.rs src/math.rs src/command.rs
-
-/home/admin/projects/PlanetaryZigma/spacetime/server/target/wasm32-unknown-unknown/release/deps/spacetime_module.wasm: src/lib.rs src/math.rs src/command.rs
-
-src/lib.rs:
-src/math.rs:
-src/command.rs:
