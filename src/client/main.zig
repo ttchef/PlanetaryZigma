b30adf94aec724cef4f22e2f9a94936f5e6f246a
HaraldWik <wikhallharald@gmail.com>
2026-02-20 21:20:21 +0100

    Test networking

--- src/client/main.zig
+++ src/client/main.zig
@@ -1,6 +1,27 @@
 const std = @import("std");
 const shared = @import("shared");
 
-pub fn main() !void {
-    std.debug.print("Client! {s}\n", .{shared.test_string});
+pub fn main(init: std.process.Init) !void {
+    std.debug.print("client\n", .{});
+    const io = init.io;
+    _ = try io.sleep(.fromSeconds(1), .real);
+    const stream = try shared.net.address.connect(io, .{ .mode = .stream });
+    defer stream.close(io);
+
+    var stream_reader_buffer: [128]u8 = undefined;
+    var stream_reader = stream.reader(io, &stream_reader_buffer);
+    const reader = &stream_reader.interface;
+
+    while (true) {
+        reader.fillMore() catch |err| switch (err) {
+            error.EndOfStream => break,
+            else => return err,
+        };
+
+        const read = reader.buffered();
+
+        std.debug.print("from server: {s}\n", .{read});
+
+        reader.tossBuffered();
+    }
 }
