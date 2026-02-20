0d8118394d54d8771c4d9a7b5fe53c228fe1b646
HaraldWik <wikhallharald@gmail.com>
2026-02-20 23:12:13 +0100

    added glfw

--- build.zig
+++ build.zig
@@ -1,11 +1,9 @@
 const std = @import("std");
 
-// TODO(ernesto): We should generate docs from our code. See how Zig std does it.
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // TODO(ernesto): Shared should be separated into it's own compilation unit so we can hot reload.
     const shared = b.addModule("shared", .{
         .root_source_file = b.path("src/shared/root.zig"),
         .target = target,
@@ -33,70 +31,33 @@ pub fn build(b: *std.Build) void {
 
 // TODO(ernesto): HOT RELOADING
 pub fn client(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode, shared: *std.Build.Module) *std.Build.Step.Compile {
-    const platform_api = b.addTranslateC(.{
-        .root_source_file = b.path("src/client/platform_api.h"),
+    const glfw_headers = b.dependency("glfw_headers", .{});
+    const glfw_translate_c = b.addTranslateC(.{
+        .root_source_file = glfw_headers.path("include/GLFW/glfw3.h"),
+        // b.addWriteFiles().add("c.h",
+        // \\#define GLFW_INCLUDE_NONE
+        // \\#include <GLFW/glfw3.h>
+        // ),
         .target = target,
         .optimize = optimize,
-    }).createModule();
+    });
 
-    const exe = switch (target.result.os.tag) {
-        .linux => exe_block: {
-            const wayland = b.option(bool, "wayland", "Use wayland windowing system") orelse false;
+    const exe = b.addExecutable(.{
+        .name = "server",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/client/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "shared", .module = shared },
+                .{ .name = "glfw", .module = glfw_translate_c.createModule() },
+            },
+        }),
+    });
 
-            const exe = b.addExecutable(.{
-                .name = if (wayland) "client-wayland" else "client-xorg",
-                .root_module = b.createModule(.{
-                    .root_source_file = b.path(if (wayland) "src/client/wayland.zig" else "src/client/xorg.zig"),
-                    .target = target,
-                    .optimize = optimize,
-                    .imports = &.{
-                        .{ .name = "shared", .module = shared },
-                    },
-                    .link_libc = true,
-                }),
-            });
-            // NOTE(ernesto): Should we move to our own headers so we don't need libc?
-            exe.root_module.linkSystemLibrary("vulkan", .{});
-            exe.root_module.addImport("platform_api", platform_api);
-            if (wayland) {
-                const vulkan_and_stuff = b.addTranslateC(.{
-                    .root_source_file = b.addWriteFiles().add("c.h",
-                        \\#include <wayland-client.h>
-                        \\#include <xdg-shell.h>
-                        \\#include <xkbcommon/xkbcommon.h>
-                        \\#include <vulkan/vulkan.h>
-                        \\#include <vulkan/vulkan_wayland.h>
-                    ),
-                    .target = target,
-                    .optimize = optimize,
-                }).createModule();
-                exe.root_module.addImport("vulkanAndStuff", vulkan_and_stuff);
-                exe.root_module.linkSystemLibrary("wayland-client", .{});
-                exe.root_module.linkSystemLibrary("xkbcommon", .{});
-            } else {
-                const xcb_and_stuff = b.addTranslateC(.{
-                    .root_source_file = b.addWriteFiles().add("c.h",
-                        \\#include <xcb/xcb.h>
-                        \\#include <xcb/xcb_keysyms.h>
-                        \\#include <vulkan/vulkan.h>
-                        \\#include <vulkan/vulkan_wayland.h>
-                        \\#include <xcb/xcb_icccm.h>
-                    ),
-                    .target = target,
-                    .optimize = optimize,
-                }).createModule();
-                exe.root_module.addImport("xcbAndStuff", xcb_and_stuff);
-                exe.root_module.linkSystemLibrary("xcb", .{});
-                exe.root_module.linkSystemLibrary("xcb-keysyms", .{});
-                exe.root_module.linkSystemLibrary("xcb-icccm", .{});
-            }
-            b.installArtifact(exe);
-            break :exe_block exe;
-        },
-        else => {
-            std.debug.panic("Compilation not implemented for target OS: {any}\n", .{target.result.os.tag});
-        },
-    };
+    exe.root_module.linkLibrary(b.dependency("glfw", .{ .target = target, .optimize = optimize }).artifact("glfw3"));
+
+    b.installArtifact(exe);
 
     return exe;
 }
--- src/client/main.zig
+++ src/client/main.zig
@@ -1,27 +1,42 @@
 const std = @import("std");
 const shared = @import("shared");
+const glfw = @import("glfw");
 
 pub fn main(init: std.process.Init) !void {
-    std.debug.print("client\n", .{});
-    const io = init.io;
-    _ = try io.sleep(.fromSeconds(1), .real);
-    const stream = try shared.net.address.connect(io, .{ .mode = .stream });
-    defer stream.close(io);
+    _ = glfw.glfwInit();
+    defer glfw.glfwTerminate();
 
-    var stream_reader_buffer: [128]u8 = undefined;
-    var stream_reader = stream.reader(io, &stream_reader_buffer);
-    const reader = &stream_reader.interface;
+    glfw.glfwWindowHint(glfw.GLFW_NO_API, glfw.GLFW_TRUE)
 
-    while (true) {
-        reader.fillMore() catch |err| switch (err) {
-            error.EndOfStream => break,
-            else => return err,
-        };
+    const window: *glfw.GLFWwindow = glfw.glfwCreateWindow(900, 800, "PlanetaryZigma", null, null) orelse return error.CreateWindow;
+    defer glfw.glfwDestroyWindow(window);
 
-        const read = reader.buffered();
+    while (glfw.glfwWindowShouldClose(window) != glfw.GLFW_TRUE) {
 
-        std.debug.print("from server: {s}\n", .{read});
-
-        reader.tossBuffered();
     }
+
+    _ = init;
+
+    // std.debug.print("client\n", .{});
+    // const io = init.io;
+    // _ = try io.sleep(.fromSeconds(1), .real);
+    // const stream = try shared.net.address.connect(io, .{ .mode = .stream });
+    // defer stream.close(io);
+
+    // var stream_reader_buffer: [128]u8 = undefined;
+    // var stream_reader = stream.reader(io, &stream_reader_buffer);
+    // const reader = &stream_reader.interface;
+
+    // while (true) {
+    //     reader.fillMore() catch |err| switch (err) {
+    //         error.EndOfStream => break,
+    //         else => return err,
+    //     };
+
+    //     const read = reader.buffered();
+
+    //     std.debug.print("from server: {s}\n", .{read});
+
+    //     reader.tossBuffered();
+    // }
 }
